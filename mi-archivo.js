0ce32ec0dc54010b325b75f40b6a442ece96450c
odobles-cenfo <odoblesp@ucenfotec.ac.cr>
2025-11-15 11:16:34 -0600

    Update mi-archivo.js
    Guardar cambios desde Github Desktop

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
     System.out.println("¡Hola, mundo desde Java!");
-    saludar("Johel");
+    saludar("Oscar");
     sumar(5, 3);
     }
    public static void saludar(String nombre) {
